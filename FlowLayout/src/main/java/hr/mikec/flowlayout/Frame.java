9f6816ee2aa227a9effbbe415565ccf19b9e4ac2
Ivan Mikec <mikecivan0@gmail.com>
2021-09-09 13:31:22 +0200

    FlowLayout example

---
+++ FlowLayout/src/main/java/hr/mikec/flowlayout/Frame.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hr.mikec.flowlayout;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Frame {
+
+    public Frame() {
+        load();
+    }
+
+    private void load() {
+       
+        //Flow Layout places components in a row, sized at their preferred size.
+        // If the horizontal space in the container is to small,
+        // the Flow Layout class uses the next available row.
+        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        
+        
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(250,250));
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setLayout(new FlowLayout());
+        
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+        
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    
+}
